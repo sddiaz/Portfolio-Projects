78869cc2f5b53fddb5109a0d521706dd65a06979
Santiago Diaz <sdiaz@niar.wichita.edu>
2022-10-29 22:42:06 -0500

    Added styling and UI elements

--- Portfolio_PasswordGenerator/scripts.js
+++ Portfolio_PasswordGenerator/scripts.js
@@ -1,1 +1,9 @@
-console.log(fetch('https://icanhazdadjoke.com/')) 
+function copyToClipboard() {   
+  // Grab our element
+  var passwordText = document.getElementById("password");
+  // Select the text in element
+  passwordText.select();
+  passwordText.setSelectionRange(0, 99999); 
+   // Copy the text top clipboard
+  navigator.clipboard.writeText(passwordText.value);
+}

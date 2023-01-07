ff37814f7102a8a5c6b7435221d8d6f7d282eee9
Santiago Diaz <sdiaz@niar.wichita.edu>
2023-01-06 20:42:30 -0600

    Finished Project

--- Portfolio_Learning_React/Mini Travel Journal/components/Footer.js
+++ Portfolio_Learning_React/Mini Travel Journal/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
     return (
       <div className="footer">
-            <footer> Built with ❤ by Santiago Diaz </footer>
+            <footer> Built with ❤ by <a   target="_blank" rel="noopener noreferrer" href="https://github.com/sddiaz">Santiago Diaz</a></footer>
       </div>  
     );
 }
